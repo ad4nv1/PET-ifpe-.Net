a3463e41cf8ce8d5bc571629d644941f4b9b4cd1
Adan Vinicius <viniciusadan123@gmail.com>
2021-09-15 14:01:36 -0300

    Primeiro commit

---
+++ PET-ifpe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PET_ifpe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Adan Vinícius!");
+        }
+    }
+}
